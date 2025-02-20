6d0376ab82fa32add225238f80b4f016c073f184
umeh-promise <umehpromise1000@gmail.com>
2025-02-20 14:29:26 +0100

    feat: add Sofwave and Microneedling components, enhance IPL component with detailed descriptions and images

--- app/services/energy-devices/page.tsx
+++ app/services/energy-devices/page.tsx
@@ -1,9 +1,10 @@
 import { Fragment } from "react";
-import Wrapper from "@/app/components/Wrapper";
 import ServicesHero from "../components/services-hero";
 import ServicesTab from "../components/services-tab";
 import IPL from "./components/ipl";
 import AgeJect from "./components/ageject";
+import Sofwave from "./components/sofwave";
+import Miconeedling from "./components/miconeedling";
 
 export default function page() {
   return (
@@ -11,7 +12,9 @@ export default function page() {
       <ServicesHero />
       <ServicesTab />
       <AgeJect />
+      <Sofwave />
       <IPL />
+      <Miconeedling />
     </Fragment>
   );
 }
