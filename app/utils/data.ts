8251f3ffaa718c0d61f2cc8bb2523a3f91835e32
umeh-promise <umehpromise1000@gmail.com>
2025-02-21 10:00:50 +0100

    feat: add benefits and FAQs for dermal fillers, Botox, and other treatments; update Tailwind config with grey-800

--- app/utils/data.ts
+++ app/utils/data.ts
@@ -24,6 +24,14 @@ export const sofwaveBenefits = [
   "Simple, intuitive, and fast treatment — improves practice patient workflow",
 ];
 
+export const dermalFillersBenefits = [
+  " Instantly restores lost volume for a youthful appearance",
+  "Smooths fine lines and wrinkles",
+  "Enhances and defines facial features",
+  "Minimally invasive with little to no downtime",
+  "Natural-looking and long-lasting results",
+];
+
 export const sofwaveSatisfactions = [
   " Non-invasive, no interruption to daily routine",
   "Fast treatment: ~30 to 45 minutes",
@@ -147,6 +155,136 @@ export const miconeedlingFAQs: IFaq[] = [
   },
   {
     title: "Things to know before your appointment?",
+    content: "",
+  },
+];
+export const botoxFAQs: IFaq[] = [
+  {
+    title: "What are Botox & Dysport?",
+    content:
+      "A concierge practice (also called concierge medicine or membership-based healthcare) is a healthcare model where patients pay a monthly or annual retainer fee to a doctor or medical practice in exchange for enhanced, personalized care. This model typically allows for longer appointment times, direct access to physicians, same-day or next-day appointments, and more comprehensive",
+  },
+  {
+    title: "How long until I see results?",
+    content: "",
+  },
+  {
+    title: "How long will the results last? ",
+    content: "",
+  },
+  {
+    title: "Are there any common side effects? ",
+    content: "",
+  },
+];
+export const dermalFillersFAQs: IFaq[] = [
+  {
+    title: "How do dermal fillers work?",
+    content:
+      "A concierge practice (also called concierge medicine or membership-based healthcare) is a healthcare model where patients pay a monthly or annual retainer fee to a doctor or medical practice in exchange for enhanced, personalized care. This model typically allows for longer appointment times, direct access to physicians, same-day or next-day appointments, and more comprehensive",
+  },
+  {
+    title: "How long do dermal fillers last?",
+    content: "",
+  },
+  {
+    title: "Is the treatment painful?",
+    content: "",
+  },
+  {
+    title: "What areas can be treated with dermal fillers?",
+    content: "",
+  },
+  {
+    title: "Is there any downtime after the procedure?",
+    content: "",
+  },
+  {
+    title: " Are dermal fillers safe?",
+    content: "",
+  },
+];
+export const sculptraFAQs: IFaq[] = [
+  {
+    title: "What is collagen?",
+    content:
+      "A concierge practice (also called concierge medicine or membership-based healthcare) is a healthcare model where patients pay a monthly or annual retainer fee to a doctor or medical practice in exchange for enhanced, personalized care. This model typically allows for longer appointment times, direct access to physicians, same-day or next-day appointments, and more comprehensive",
+  },
+  {
+    title: "Why is collagen important?",
+    content: "",
+  },
+  {
+    title: "What can I do to restore my collagen?",
+    content: "",
+  },
+  {
+    title: "What is the treatment like?",
+    content: "",
+  },
+  {
+    title: "Are there any common side effects?",
+    content: "",
+  },
+];
+export const prrRejuvenationFAQs: IFaq[] = [
+  {
+    title: "What is a Vampire Facelift?",
+    content:
+      "A concierge practice (also called concierge medicine or membership-based healthcare) is a healthcare model where patients pay a monthly or annual retainer fee to a doctor or medical practice in exchange for enhanced, personalized care. This model typically allows for longer appointment times, direct access to physicians, same-day or next-day appointments, and more comprehensive",
+  },
+  {
+    title: "How Does the Vampire Facelift Work?",
+    content: "",
+  },
+  {
+    title: "Benefits of the Vampire Facelift?",
+    content: "",
+  },
+  {
+    title: "Who is a Good Candidate for the Vampire Facelift?",
+    content: "",
+  },
+  {
+    title: "What to Expect During and After the Procedure?",
+    content: "",
+  },
+];
+export const prpRejuvenationFAQs: IFaq[] = [
+  {
+    title: "How Many Treatments will I Need?",
+    content:
+      "A concierge practice (also called concierge medicine or membership-based healthcare) is a healthcare model where patients pay a monthly or annual retainer fee to a doctor or medical practice in exchange for enhanced, personalized care. This model typically allows for longer appointment times, direct access to physicians, same-day or next-day appointments, and more comprehensive",
+  },
+  {
+    title: "When Will I see Results?",
+    content: "",
+  },
+  {
+    title: "How long do results last?",
+    content: "",
+  },
+  {
+    title: "What is the recovery time?",
+    content: "",
+  },
+  {
+    title: "How much does PRP Treatment Cost?",
+    content: "",
+  },
+];
+export const kybellaFAQs: IFaq[] = [
+  {
+    title: "How long until I see results?",
+    content:
+      "A concierge practice (also called concierge medicine or membership-based healthcare) is a healthcare model where patients pay a monthly or annual retainer fee to a doctor or medical practice in exchange for enhanced, personalized care. This model typically allows for longer appointment times, direct access to physicians, same-day or next-day appointments, and more comprehensive",
+  },
+  {
+    title: "How long will the results last?",
+    content: "",
+  },
+  {
+    title: "Are there common side effects? ",
     content: "",
   },
 ];
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -42,6 +42,7 @@ export default {
       "grey-600": "#8E9BAE",
       "grey-700": "#282828",
       "grey-750": "#55534E",
+      "grey-800": "#16161699",
       "grey-900": "#161616",
     },
     fontSize: {
