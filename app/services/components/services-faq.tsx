8d9d3c65dd295ae79d0e8849f89d9acb4be63946
umeh-promise <umehpromise1000@gmail.com>
2025-02-21 11:39:44 +0100

    feat: update service link for Sexual Wellness and add optional className prop to ServicesFAQ component

--- app/services/components/services-faq.tsx
+++ app/services/components/services-faq.tsx
@@ -5,15 +5,17 @@ interface ServicesFAQProps {
   title: string;
   description: string;
   faqs: IFaq[];
+  className?: string;
 }
 
 export default function ServicesFAQ({
   title,
   description,
   faqs,
+  className,
 }: ServicesFAQProps) {
   return (
-    <section data-aos="fade-up">
+    <section data-aos="fade-up" className={className}>
       <hgroup className="mx-auto flex w-[100rem] flex-col gap-[2rem]">
         <h4 className="text-grey-750 text-center font-rubik text-[5.5rem] font-bold leading-[5.5rem] tracking-[0.02em]">
           {title}
