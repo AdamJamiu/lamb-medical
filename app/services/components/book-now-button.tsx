029331e0e4f43a491dad5d6c2ed49d28f11a091a
umeh-promise <umehpromise1000@gmail.com>
2025-02-22 15:10:08 +0100

    feat: refactor BookNowButton component for improved styling and class management

--- app/services/components/book-now-button.tsx
+++ app/services/components/book-now-button.tsx
@@ -1,14 +1,21 @@
+import { cn } from "@/app/utils";
 import { ButtonHTMLAttributes } from "react";
 
 interface BookNowButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
 }
 
-export default function BookNowButton({ children, ...props }: BookNowButtonProps) {
+export default function BookNowButton({
+  children,
+  ...props
+}: BookNowButtonProps) {
   return (
     <button
       {...props}
-      className={`font-poppins flex items-center justify-center rounded-[0.5rem] bg-primary px-[4.1rem] py-[1.5rem] text-lg font-semibold text-white transition hover:opacity-70 ${props.className ?? ""}`}
+      className={cn(
+        "flex items-center justify-center rounded-[0.5rem] bg-primary px-[4.1rem] py-[1.2rem] font-poppins text-md font-semibold text-white transition hover:opacity-70",
+        props.className,
+      )}
     >
       {children}
     </button>
