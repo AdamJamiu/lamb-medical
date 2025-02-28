943bca6d545014e53f68afbbf37c0b65eb02ac01
AdamJamiu <adamjamiu98@gmail.com>
2025-02-28 20:04:03 +0100

    mobile responsiveness

--- app/services/facial-services/_components/Dermaplanning.tsx
+++ app/services/facial-services/_components/Dermaplanning.tsx
@@ -4,7 +4,7 @@ import ServicesIntro from "../../components/services-intro";
 
 export default function Dermaplanning() {
   return (
-    <Wrapper id="dermaplanning" className="grid gap-[4rem] py-10">
+    <Wrapper id="dermaplanning" className="grid gap-[4rem] pt-10">
       <ServicesIntro
         title="Dermaplaning"
         description="Dermaplaning is a manual exfoliation technique the removes surface debris with a custom dermaplaning blade. Come in and experience dermaplaning as a stand alone service or add dermaplaning to a facial or chemical peel. This treatment involves using a sterile surgical blade to remove dead skin and “peach fuzz” from the complexion. This treatment is a form of physical exfoliation and also allows for skincare to penetrate deeper and for makeup to apply more smoothly. This is great as a standalone service or in conjunction with other aesthetic services. There is no downtime with dermaplaning."
@@ -34,7 +34,7 @@ export default function Dermaplanning() {
         fine lines and wrinkles will be diminished. Dermaplaning is a great way
         to prep your skin before a chemical peel.
       </p>
-      <p className="text-grey-750 font-work_sans text-[14px] xs:text-lg font-medium leading-[3.6rem] tracking-[0.02rem] opacity-80 pb-28">
+      <p className="text-grey-750 font-work_sans text-[14px] xs:text-lg font-medium leading-[3.6rem] tracking-[0.02rem] opacity-80 pb-4">
         Dermaplaning will NOT cause hair to grow back darker or thicker nor will
         it cause breakouts. Additionally, dermaplaning is not painful. The
         treatment is performed with a specialized razor and cannot be done at
--- app/services/facial-services/_components/Peels.tsx
+++ app/services/facial-services/_components/Peels.tsx
@@ -39,7 +39,7 @@ export default function Peels() {
         </li>
       </ul>
 
-      <p className="text-grey-750 font-work_sans text-[14px] xs:text-lg font-medium leading-[3.6rem] tracking-[0.02rem] opacity-80 pb-28">
+      <p className="text-grey-750 font-work_sans text-[14px] xs:text-lg font-medium leading-[3.6rem] tracking-[0.02rem] opacity-80 pb-4">
         The VI Peel is a skin-resurfacing chemical peel that stimulates collagen
         and increases cellular turnover to reveal smoother and more radiant
         skin. In just 15 years, over 10 million peels have been performed
@@ -67,7 +67,7 @@ export default function Peels() {
           data-aos="zoom-in"
           src="/images/facial-services/vi-peels-mobile.png"
           alt="vi-peels-mobile"
-          className="xs:hidden block"
+          className="xs:hidden block h-fi"
         />
       </figure>
 
@@ -166,7 +166,7 @@ export default function Peels() {
 
         <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-10 mt-16">
           <div className="relative">
-            <h2 className="font-semibold text-white absolute top-10 right-[50%] xs:text-[47.54px] text-[32px]">
+            <h2 className="font-semibold text-white absolute top-10 right-[50%] xs:text-[47.54px] text-[32px] z-10">
               Before
             </h2>
 
@@ -178,8 +178,8 @@ export default function Peels() {
             />
           </div>
           <div className="relative">
-            <h2 className="font-semibold text-white absolute top-10 right-[50%] xs:text-[47.54px] text-[32px]">
-              Before
+            <h2 className="font-semibold text-white absolute top-10 right-[50%] xs:text-[47.54px] text-[32px] z-10">
+              After
             </h2>
             <img
               src="/images/facial-services/eye-after-1.png"
@@ -190,7 +190,7 @@ export default function Peels() {
           </div>
 
           <div className="relative">
-            <h2 className="font-semibold text-white absolute top-10 right-[50%] xs:text-[47.54px] text-[32px]">
+            <h2 className="font-semibold text-white absolute top-10 right-[50%] xs:text-[47.54px] text-[32px] z-10">
               Before
             </h2>
             <img
@@ -202,7 +202,7 @@ export default function Peels() {
           </div>
 
           <div className="relative">
-            <h2 className="font-semibold text-white absolute top-10 right-[50%] xs:text-[47.54px] text-[32px]">
+            <h2 className="font-semibold text-white absolute top-10 right-[50%] xs:text-[47.54px] text-[32px] z-10">
               After
             </h2>
             <img
